d517ce4fd9b3840749c6015f1e3297dcc7cb4658
jrebiffe <jean.rebiffe@gmail.com>
2024-12-24 21:56:30 +0100

    Adding "full" status report

--- game.py
+++ game.py
@@ -1,9 +1,16 @@
 """Connect Four game for mulitplayer Reinforcement Learning trial."""
 
-from itertools import cycle
+from typing import TypedDict
 
 from numpy import count_nonzero, diagonal, int_, ndarray, zeros
 from numpy.lib.stride_tricks import sliding_window_view
+
+
+class MoveStatus(TypedDict):
+    """Report the game status after `Game.step()`."""
+    illegal: bool
+    win: bool
+    full: bool
 
 
 class Game:
@@ -12,25 +19,25 @@ class Game:
     def __init__(self) -> None:
         # TODO(Jean): Variablize board size (Fanny's suggestion)
         self.board = zeros((7, 7), dtype=int_)
-        self.players = cycle((1, 2))
 
     def observe(self) -> ndarray:
         """Read the current board state."""
         return self.board
 
-    def step(self, action: dict[str, int]) -> dict[str, bool]:
+    def step(self, action: dict[str, int]) -> MoveStatus:
         """Play a single move."""
-        # player_id = next(self.players)
         print(action)
-        player_id = action['player_id']
+        player_id = action["player_id"]
         column = action["column"]
+
         height = count_nonzero(self.board[column, :])
         if height == self.board[column, :].size:
-            return {"illegal": True, "win": False}
+            return MoveStatus(illegal=True, win=False, full=False)
         self.board[column, height] = player_id
 
         win = self.is_win(column, height)
-        return {"illegal": False, "win": win}
+        full = (self.board == 0).sum() == 0
+        return MoveStatus(illegal=False, win=win, full=full)
 
     def is_win(self, column, height) -> bool:
         """Verify is the given coin has scored."""
