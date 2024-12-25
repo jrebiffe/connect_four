8865377c8fa07425c56f06c219c513229f9dd2da
jrebiffe <jean.rebiffe@gmail.com>
2024-12-25 23:24:07 +0100

    Add color in human_agent.py

---
+++ agent/human_agent.py
@@ -0,0 +1,44 @@
+"""Connect Four human player interface to face a RL agent."""
+
+from numpy import rot90
+
+class HumanAgent:
+    """Connect Four human player interface."""
+
+    def call_action(self) -> int:
+        """Ask human the column in which to insert coin."""
+        value: int | None = None
+        while value is None:
+            text = input("In which columns?")
+            try:
+                value = int(text)
+            except ValueError as exception:
+                print(f"'{text}' is not an int: {exception}")
+            if value not in range(7):
+                print(f"'{value}' is not in range(7)")
+                value = None
+
+        return value
+
+    def render(self, board) -> None:
+        """Display board to the human."""
+        board = rot90(board)
+        print(" ", end="")
+        for index, line in enumerate(board[0, :]):
+            print(index, end="")
+        print()
+
+        for line in board:
+            print("|", end="")
+            for column in line:
+                if column == 0:
+                    print(" ", end="")
+                elif column == 1:
+                    print("\033[91mX\033[0m", end="")
+                elif column == 2:
+                    print("\033[93mO\033[0m", end="")
+            print("|")
+
+    def result(self, new_obs, reward, done, info) -> None:
+        """Display RL agent's `step()` output to the human."""
+        print(new_obs, reward, done, info)
